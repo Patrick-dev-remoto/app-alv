d86e61af69bfead2df586d2de2a20a9c4a94c005
Patrick-dev-remoto <patrick.dias123xz@gmail.com>
2024-09-24 10:18:45 -0400

    + Adjusts in the overflow

--- system_alv/front-alv/src/components/ui/commandDemo.tsx
+++ system_alv/front-alv/src/components/ui/commandDemo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Calculator,
   Calendar,
@@ -5,6 +6,7 @@ import {
   Settings,
   Smile,
   User,
+  Search,
 } from "lucide-react";
 
 import {
@@ -17,50 +19,71 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
-import { useState } from "react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 export function CommandDemo() {
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState(false);
+
   return (
-    <Command className="rounded-lg border md:min-w-[450px]">
-      <CommandInput placeholder="Type a command or search..." />
-      {open && (
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <Calendar className="mr-2 h-4 w-4" />
-              <span>Calendar</span>
-            </CommandItem>
-            <CommandItem>
-              <Smile className="mr-2 h-4 w-4" />
-              <span>Search Emoji</span>
-            </CommandItem>
-            <CommandItem disabled>
-              <Calculator className="mr-2 h-4 w-4" />
-              <span>Calculator</span>
-            </CommandItem>
-          </CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="Settings">
-            <CommandItem>
-              <User className="mr-2 h-4 w-4" />
-              <span>Profile</span>
-              <CommandShortcut>⌘P</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-              <CreditCard className="mr-2 h-4 w-4" />
-              <span>Billing</span>
-              <CommandShortcut>⌘B</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-              <Settings className="mr-2 h-4 w-4" />
-              <span>Settings</span>
-              <CommandShortcut>⌘S</CommandShortcut>
-            </CommandItem>
-          </CommandGroup>
-        </CommandList>
-      )}
-    </Command>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          className="w-full justify-start text-left font-normal focus-visible:outline-none focus-within:outline-none shadow-sm"
+        >
+          <Search className="mr-2 h-4 w-4" />
+          <span>Search...</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0">
+        <DialogHeader>
+          <DialogTitle className="hidden"></DialogTitle>
+          <DialogDescription className="hidden"></DialogDescription>
+        <Command className="rounded-lg">
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>
+                <Calendar className="mr-2 h-4 w-4" />
+                <span>Calendar</span>
+              </CommandItem>
+              <CommandItem>
+                <Smile className="mr-2 h-4 w-4" />
+                <span>Search Emoji</span>
+              </CommandItem>
+              <CommandItem disabled>
+                <Calculator className="mr-2 h-4 w-4" />
+                <span>Calculator</span>
+              </CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+                <CommandShortcut>⌘P</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <CreditCard className="mr-2 h-4 w-4" />
+                <span>Billing</span>
+                <CommandShortcut>⌘B</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <CommandShortcut>⌘S</CommandShortcut>
+              </CommandItem>
+              <CommandItem onSelect={() => setOpen(false)}>
+                <span>Close</span>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
--- system_alv/front-alv/src/pages/Home.tsx
+++ system_alv/front-alv/src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import Header from "@/components/header";
+import ItemsGrid from "@/components/itemsgrid";
 import Sidebar from "@/components/sidebar";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <div className="pl-[250px] flex-1 flex flex-col h-full w-screen">
+      <div className="flex-1 flex flex-col h-screen overflow-auto w-screen">
         <Header />
-        <main className="flex-1 p-4 overflow-x-auto max-h-[40.3rem] scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 scrollbar-thumb-rounded">
-          <Button variant={"outline"}>Click me man</Button>
+        <main className="flex-1 p-4">
+          <ItemsGrid></ItemsGrid>
         </main>
       </div>
     </div>
